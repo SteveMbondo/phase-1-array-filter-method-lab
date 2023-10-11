dd7f135f6124a94dd21f3059e6ac73280c961941
root <+>
2023-10-11 08:01:29 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(drivers, nameQuery) {
+    const lowercaseQuery = nameQuery.toLowerCase();
+    return drivers.filter(driver => driver.toLowerCase() === lowercaseQuery);
+  }
+  function fuzzyMatch(drivers, nameQuery) {
+    return drivers.filter(driver => driver.startsWith(nameQuery));
+  }
+  function matchName(drivers, nameQuery) {
+    return drivers.filter(driver => driver.name === nameQuery);
+  }
